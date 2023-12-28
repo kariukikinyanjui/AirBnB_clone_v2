b44d6351fd12f2ac8761e34d40c7e0d1dbf53ae1
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-12-28 21:15:06 +0000

    Prepare my web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Install Nginx
+sudo apt-get update
+sudo apt-get -y nginx
+
+# Create directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create HTML file to test your Nginx configuration
+echo "Hello Mike!" | sudo tee /data/web_static/releases/test/index.html
+
+# Create or recreate symbolic link
+sudo ln -sf /data/web_static/release/test /data/web_static/current
+
+# Give ownership to the ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data
+
+# Update Nginx configuration
+config_file='/etc/nginx/sites-available/default'
+sudo sed -i '/location \/hbnb_static {/!b;n;c\\talias /data/web_static/current/;' "$config_file"
+
+sudo service nginx restart
