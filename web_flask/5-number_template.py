083166c1720172d452c5272b13aa98f79b46cd69
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-01-22 19:16:49 +0000

    [Feature]: Updated route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,9 +35,10 @@ def number_route(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_route(n):
-    n = str(n)
-    return render_template('5-number_template.html', number=n)
-
+    try:
+        return render_template('5-number_template.html', n=n)
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
